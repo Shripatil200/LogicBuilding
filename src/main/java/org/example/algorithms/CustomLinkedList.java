d9a814beda126ada5b44766488f8a80870b26832
Shripatil200 <patilshriram200@gmail.com>
2026-01-31 09:26:00 +0530

    Customized Linked List

---
+++ src/main/java/org/example/algorithms/CustomLinkedList.java
@@ -0,0 +1,273 @@
+package org.example.algorithms;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class CustomLinkedList<E>  {
+    transient int size = 0;
+    transient Node<E> first;
+    transient Node<E> last;
+
+
+
+    private static class Node<E>{
+
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next){
+            this.item = element;
+            this.prev = prev;
+            this.next = next;
+        }
+
+    }
+
+
+
+    public CustomLinkedList(){
+    }
+
+    public CustomLinkedList(Collection<? extends E> c){
+        this();
+        addAll(c);
+    }
+
+
+
+
+
+    void linkLast(E e){
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+
+        if(l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+
+        size++;
+    }
+
+
+
+    E unlink(Node<E> x){
+        E element = x.item;
+        Node<E> next = x.next;
+        Node<E> prev = x.prev;
+
+        if(prev == null)
+            first = next;
+        else{
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if(next == null)
+            last = prev;
+        else{
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size --;
+        return element;
+    }
+
+    E unlinkFirst(Node<E> f){
+        final E element = f.item;
+        final Node<E> next = f.next;
+        f.item = null;
+        f.next = null;
+        first = next;
+        if(next == null)
+            last = null;
+        else
+            next.prev = null;
+
+        size--;
+        return element;
+    }
+
+    E unlinkLast(Node<E> l){
+        final E element = l.item;
+        final Node<E> prev = l.prev;
+        l.item = null;
+        l.prev = null;
+        last = prev;
+
+        if(prev == null){
+            first = null;
+        }
+        else{
+            prev.next = null;
+        }
+        size--;
+        return element;
+    }
+
+
+
+    public E getFirst(){
+        Node<E> f = first;
+        if(f == null)
+            throw new NoSuchElementException();
+        return f.item;
+    }
+
+    public E getLast(){
+        Node<E> l = last;
+        if(l == null)
+            throw new NoSuchElementException();
+        return l.item;
+    }
+
+
+    public E removeFirst(){
+        final Node<E> f = first;
+        if(f == null)
+            throw new NoSuchElementException();
+        return unlinkFirst(f);
+    }
+
+    public E removeLast(){
+        final Node<E> l = last;
+        if(l == null)
+            throw new NoSuchElementException();
+        return unlinkLast(l);
+    }
+
+
+
+    public boolean add(E element){
+        linkLast(element);
+        return true;
+    }
+
+
+    public boolean remove(Object o){
+        if(o == null){
+            for(Node<E> x = first; x != null; x = x.next){
+                if(x.item == null){
+                    unlink(x);
+                    return true;
+                }
+            }
+        }else{
+            for(Node<E> x = first; x != null; x = x.next){
+                if(o.equals(x.item)){
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean addAll(Collection<? extends E> c){ return addAll(size, c);}
+
+    public boolean addAll(int index, Collection<? extends E> c){
+        checkPositionIndex(index);
+
+        Object[] a = c.toArray();
+        int numNew = a.length;
+
+        if(numNew == 0)
+            return false;
+
+        Node<E> pred, suss;
+
+        if(index == size){
+            suss = null;
+            pred = last;
+        }else{
+            suss = node(index);
+            pred = suss.prev;
+        }
+
+        for(Object o : a){
+            E e = (E) o;
+            Node<E> newNode = new Node<>(pred, e , null);
+            if(pred == null)
+                first = newNode;
+            else
+                pred.next = newNode;
+            pred = newNode;
+
+        }
+
+        if(suss == null){
+            last = pred;
+        }else{
+            pred.next = suss;
+            suss.prev = pred;
+        }
+
+        size += numNew;
+        return true;
+    }
+
+
+    private void checkPositionIndex(int index){
+        if(!isPositionIndex(index))
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private boolean isPositionIndex(int index){
+        return index >= 0 && index <= size;
+    }
+
+    Node<E> node(int index){
+        if(index < (size >> 1)){
+            Node<E> x = first;
+            for(int i = 0; i < index; i++){
+                x = x.next;
+            }
+            return x;
+        }else{
+            Node<E> x = last;
+            for(int i = size -1; i > index; i--){
+                x = x.prev;
+            }
+            return x;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("[");
+        for (Node<E> e = first; e != null; e = e.next) {
+            str.append(e.item);
+            str.append(e.next == null ? "]" : ", ");
+        }
+       return str.toString();
+    }
+
+
+
+
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+
+
+
+}
---
+++ src/main/java/org/example/algorithms/Test.java
@@ -0,0 +1,43 @@
+package org.example.algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        CustomLinkedList<Integer> list = new CustomLinkedList<>();
+//        list.add(23);
+//        list.add(21);
+//
+//        System.out.println(list);
+//
+//        list.add(45);
+//        list.add(46);
+//        System.out.println(list);
+//
+//        list.remove(23);
+//        System.out.println(list);
+//        list.remove(56);
+//        System.out.println(list);
+//
+//        System.out.println(list.size);
+//
+        List<Integer> l1 = Arrays.asList(23,45,12,45,65);
+//
+//        list.addAll(2, l1 );
+//        System.out.println(list);
+//
+//        list.addAll(l1);
+//        System.out.println(list);
+
+        CustomLinkedList<Integer> list2  = new CustomLinkedList<>(l1);
+
+        System.out.println(list2);
+
+        System.out.println(list2.getFirst());
+        System.out.println(list2.getLast());
+    }
+}
