2c42fb188c994bf613625c187a6863d41b0a1c1a
Shripatil200 <patilshriram200@gmail.com>
2026-02-05 18:30:56 +0530

    Sliding Window and DP Arithmetic Slice

---
+++ src/main/java/org/example/algorithms/slidingwindow/ArithmeticSlice.java
@@ -0,0 +1,41 @@
+package org.example.algorithms.slidingwindow;
+
+
+import java.util.Arrays;
+
+/*
+413. Arithmetic Slices
+Medium
+
+https://leetcode.com/problems/arithmetic-slices/description/
+
+An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+Given an integer array nums, return the number of arithmetic subarrays of nums.
+
+A subarray is a contiguous subsequence of the array.
+ */
+public class ArithmeticSlice {
+
+    public static int numberOfArithmeticSlices(int[] nums) {
+        if (nums.length < 3) return 0;
+
+        int[] arr = new int[nums.length];
+
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+        return Arrays.stream(arr).sum();
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,7,9};
+
+        System.out.println(numberOfArithmeticSlices(nums));
+    }
+}
+
